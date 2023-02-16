4bc8a20d112b6c0573fe278bc7581dda76d541a2
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2023-02-16 11:38:20 +0100

    updated car test file to controll the pi car

--- src/TSR/src/car.py
+++ src/TSR/src/car.py
@@ -2,36 +2,28 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin-Definitionen
-MOTOR_A = (17, 4) # IN1 und IN2
-MOTOR_B = (27, 5) # IN3 und IN4
+MOTOR_A = 17 # IN1 und IN2
+MOTOR_B = 27# IN3 und IN4
 STEERING = 0
 
 # Pin-Setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOTOR_A[0], GPIO.OUT)
-GPIO.setup(MOTOR_A[1], GPIO.OUT)
-GPIO.setup(MOTOR_B[0], GPIO.OUT)
-GPIO.setup(MOTOR_B[1], GPIO.OUT)
+GPIO.setup(MOTOR_A, GPIO.OUT)
+GPIO.setup(MOTOR_B, GPIO.OUT)
 GPIO.setup(STEERING, GPIO.OUT)
 
 # Funktionen zum Vorwärtsfahren, Rückwärtsfahren und Anhalten
 def forward():
-    GPIO.output(MOTOR_A[0], GPIO.HIGH)
-    GPIO.output(MOTOR_A[1], GPIO.LOW)
-    GPIO.output(MOTOR_B[0], GPIO.HIGH)
-    GPIO.output(MOTOR_B[1], GPIO.LOW)
+    GPIO.output(MOTOR_A, GPIO.HIGH)
+    GPIO.output(MOTOR_B, GPIO.HIGH)
 
 def reverse():
-    GPIO.output(MOTOR_A[0], GPIO.LOW)
-    GPIO.output(MOTOR_A[1], GPIO.HIGH)
-    GPIO.output(MOTOR_B[0], GPIO.LOW)
-    GPIO.output(MOTOR_B[1], GPIO.HIGH)
+    GPIO.output(MOTOR_A, GPIO.LOW)
+    GPIO.output(MOTOR_B, GPIO.LOW)
 
 def stop():
-    GPIO.output(MOTOR_A[0], GPIO.LOW)
-    GPIO.output(MOTOR_A[1], GPIO.LOW)
-    GPIO.output(MOTOR_B[0], GPIO.LOW)
-    GPIO.output(MOTOR_B[1], GPIO.LOW)
+    GPIO.output(MOTOR_A, GPIO.LOW)
+    GPIO.output(MOTOR_B, GPIO.LOW)
 
 def steer(angle):
     duty_cycle = (angle / 180.0) * 10.0 + 2.5
