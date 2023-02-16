251c63ee0232e20373334bf68b8053db020e90b9
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2023-02-16 11:19:25 +0100

    added car test file to controll the pi car

---
+++ src/TSR/src/car.py
@@ -0,0 +1,66 @@
+import RPi.GPIO as GPIO
+import time
+
+# Pin-Definitionen
+MOTOR_A = (13, 19) # IN1 und IN2
+MOTOR_B = (16, 26) # IN3 und IN4
+STEERING = 20
+
+# Pin-Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MOTOR_A[0], GPIO.OUT)
+GPIO.setup(MOTOR_A[1], GPIO.OUT)
+GPIO.setup(MOTOR_B[0], GPIO.OUT)
+GPIO.setup(MOTOR_B[1], GPIO.OUT)
+GPIO.setup(STEERING, GPIO.OUT)
+
+# Funktionen zum Vorwärtsfahren, Rückwärtsfahren und Anhalten
+def forward():
+    GPIO.output(MOTOR_A[0], GPIO.HIGH)
+    GPIO.output(MOTOR_A[1], GPIO.LOW)
+    GPIO.output(MOTOR_B[0], GPIO.HIGH)
+    GPIO.output(MOTOR_B[1], GPIO.LOW)
+
+def reverse():
+    GPIO.output(MOTOR_A[0], GPIO.LOW)
+    GPIO.output(MOTOR_A[1], GPIO.HIGH)
+    GPIO.output(MOTOR_B[0], GPIO.LOW)
+    GPIO.output(MOTOR_B[1], GPIO.HIGH)
+
+def stop():
+    GPIO.output(MOTOR_A[0], GPIO.LOW)
+    GPIO.output(MOTOR_A[1], GPIO.LOW)
+    GPIO.output(MOTOR_B[0], GPIO.LOW)
+    GPIO.output(MOTOR_B[1], GPIO.LOW)
+
+def steer(angle):
+    duty_cycle = (angle / 180.0) * 10.0 + 2.5
+    pwm = GPIO.PWM(STEERING, 50)
+    pwm.start(duty_cycle)
+    time.sleep(0.5)
+    pwm.stop()
+
+# Testfahrt
+try:
+    while True:
+        # Vorwärtsfahren für 2 Sekunden
+        forward()
+        steer(90)
+        time.sleep(2)
+
+        # Anhalten für 1 Sekunde
+        stop()
+        time.sleep(1)
+
+        # Rückwärtsfahren für 2 Sekunden
+        reverse()
+        steer(45)
+        time.sleep(2)
+
+        # Anhalten für 1 Sekunde
+        stop()
+        time.sleep(1)
+
+# Programm beenden mit Strg + C
+except KeyboardInterrupt:
+    GPIO.cleanup()
